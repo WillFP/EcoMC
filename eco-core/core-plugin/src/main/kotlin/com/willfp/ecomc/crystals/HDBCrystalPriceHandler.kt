b23699b423d61914fd2c8b29d503f5e44cba2b92
Auxilor <william.favierparsons1@gmail.com>
2022-05-03 21:28:03 +0100

    Improved HDB integration

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecomc/crystals/HDBCrystalPriceHandler.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecomc/crystals/HDBCrystalPriceHandler.kt
@@ -2,7 +2,9 @@ package com.willfp.ecomc.crystals
 
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.drops.DropQueue
+import com.willfp.eco.core.fast.fast
 import me.arcaniax.hdb.api.PlayerClickHeadEvent
+import org.bukkit.Sound
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
@@ -22,7 +24,16 @@ class HDBCrystalPriceHandler(private val plugin: EcoPlugin) : Listener {
             return
         }
 
-        player.sendMessage(plugin.langYml.getMessage("bought-head"))
+        player.sendMessage(
+            plugin.langYml.getMessage("bought-from-crystal")
+                .replace("%item%", event.head.fast().displayName)
+        )
+        player.playSound(
+            player.location,
+            Sound.BLOCK_NOTE_BLOCK_PLING,
+            1f,
+            1.5f
+        )
         player.crystals -= 12
         DropQueue(player)
             .addItem(event.head)
