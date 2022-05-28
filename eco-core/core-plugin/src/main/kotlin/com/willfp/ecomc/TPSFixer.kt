60e4f12a9fe8ecc46bf3ae7396f2a306fc2abe3b
Auxilor <william.favierparsons1@gmail.com>
2022-05-28 14:20:16 +0100

    Made TPSFixer more aggressive

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecomc/TPSFixer.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecomc/TPSFixer.kt
@@ -4,6 +4,9 @@ import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.util.NumberUtils
 import org.bukkit.Bukkit
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.ItemFrame
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Tameable
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntitySpawnEvent
@@ -43,6 +46,8 @@ class TPSFixer : Listener {
             // Run every 10 seconds
             plugin.scheduler.runTimer(200, 200) {
                 val tps = min(Bukkit.getTPS()[0], Bukkit.getTPS()[1]) // Ignore 15m tps
+                val lastTps = Bukkit.getTPS()[0]
+
                 if (tps < 19.9) {
                     plugin.logger.info("TPS is dropping - Killing spawner mobs!")
                     for (world in Bukkit.getWorlds()) {
@@ -58,20 +63,47 @@ class TPSFixer : Listener {
                     }
                 }
 
-                spawnRate = if (tps > 18) {
+                spawnRate = if (tps > 19) {
                     1.0
-                } else if (tps > 17) {
+                } else if (tps > 18) {
                     plugin.logger.info("TPS is $tps; spawn rate = 0.5")
                     0.5
-                } else if (tps > 15) {
+                } else if (tps > 17) {
                     plugin.logger.info("TPS is $tps; spawn rate = 0.25")
                     0.25
-                } else if (tps > 10) {
+                } else if (tps > 15) {
                     plugin.logger.info("TPS is $tps; spawn rate = 0.1")
                     0.1
                 } else {
                     plugin.logger.info("TPS is $tps; spawn rate = 0")
                     0.0
+                }
+
+                if (lastTps < 18) {
+                    plugin.logger.info("Most recent TPS is $lastTps; halving mobs!")
+                    for (world in Bukkit.getWorlds()) {
+                        if (world.name.lowercase().contains("spawn")) {
+                            continue
+                        }
+
+                        for (entity in world.entities.toList()) {
+                            if (NumberUtils.randFloat(0.0, 1.0) > 0.5) {
+                                continue
+                            }
+
+                            if (entity is Tameable || entity is ItemFrame) {
+                                continue
+                            }
+
+                            if (entity is LivingEntity) {
+                                if (entity.isCustomNameVisible) {
+                                    continue
+                                }
+                            }
+
+                            entity.remove()
+                        }
+                    }
                 }
             }
         }
